bed53bfebca34a88f98edc9e0e8e1cfa609a5dbe
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-02-29 11:01:26 -0500

    Add logic to controller and directives to view

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,18 @@
 function VoteController() {
     this.votes = 0;
-
+    this.vote_count = 0
     this.incrementVotes = function () {
-
+      if (this.vote_count < 1){
+        this.votes ++
+        this.vote_count ++
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.vote_count > -1) {
+        this.votes --
+        this.vote_count --
+      }
     };
 }
 
